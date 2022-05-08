1804b382536ffd8601daaf52b3f1df8dded2e484
Austin Howard <austin.smithfield@gmail.com>
2022-05-09 00:39:38 +0100

    posts to /balances auto sort entries by timestamp

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
-const pointsData = [
-  {
-    payer: 'DANONE',
-    points: 1000, 
-    timestamp: 'Today'
-  }
-]
+let pointsData = []
 
-app.get('/balances', (req, res, next) => {
+app.get('/balances', (req, res) => {
   res.json(pointsData)
 })
 
-app.post('/balances', (req, res, next) => {
-  pointsData = req.body
+app.post('/balances', (req, res) => {
+  pointsData.push(req.body)
+  sortByTimestamp(pointsData, "timestamp")
   res.json(pointsData)
 })
 
-app.post('/spend', (req, res, next) => {
+app.post('/spend', (req, res) => {
   res.json(pointsData)
 })
 
-
+function sortByTimestamp(array, key) {
+    return array.sort(function(a, b) {
+        let x = a[key]
+        let y = b[key]
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
+}
 
 const PORT = 3000
 
